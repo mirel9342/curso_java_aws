26bcd40e3a0c9972a953f89af66ad55b4d6c0ca3
Mireli Oliveira Gonçalves <omireli300@gmail.com>
2024-11-24 20:23:02 -0300

    primeiro commit

---
+++ src/main/java/com/rocketseat/createLambda/UrlData.java
@@ -0,0 +1,16 @@
+package com.rocketseat.createLambda;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class UrlData {
+    private String originalUrl;
+    private long expirationTime;
+}
